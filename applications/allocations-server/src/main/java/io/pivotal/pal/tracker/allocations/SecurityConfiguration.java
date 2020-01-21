b8b10f7158337fdfcbe16e7139e2fc3443da9a70
Sanjaya <248453@cognizant.com>
2020-01-20 23:05:53 -0500

    cloudNativeDeveloperDistributedSystemWithServiceDiscovery

--- applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/SecurityConfiguration.java
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/SecurityConfiguration.java
@@ -1,12 +1,13 @@
 package io.pivotal.pal.tracker.allocations;
 
-import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+
+//@Configuration
 @EnableWebSecurity
-@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
--- applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/SecurityConfiguration.java
+++ applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/SecurityConfiguration.java
@@ -1,12 +1,10 @@
 package io.pivotal.pal.tracker.backlog;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
